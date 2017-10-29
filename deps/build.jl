a7bd8db33614244d141eca9962983a5884320e2d
Jonathan Bieler <jonathan.bieler@alumni.epfl.ch>
2017-10-29 18:06:44 +0100

    fix deprecation

--- deps/build.jl
+++ deps/build.jl
@@ -1,12 +1,12 @@
 println("checking for libgtksourceview...")
-@linux_only begin
+@static if is_linux()
     try
         strip(readall(pipeline(`ldconfig -p`, `grep libgtksourceview-3`, `cut -d'>' -f2`)))
     catch
         run(`sudo apt-get install libgtksourceview-3.0-1`)
     end
 end
-@osx_only begin
+@static if is_apple()
     if !isfile( Pkg.dir() * "/Homebrew/deps/usr/lib/libgtksourceview-3.0.dylib" )
         using Homebrew
         Homebrew.add("gtksourceview3")
