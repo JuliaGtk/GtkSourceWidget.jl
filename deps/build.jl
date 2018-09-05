4048eb96e1d270ceef51962e7c7dbd50700c45e3
Jonathan Bieler <jonathan.bieler@alumni.epfl.ch>
2018-09-05 17:06:56 +0200

    build script

--- deps/build.jl
+++ deps/build.jl
@@ -1,22 +1,3 @@
-println("checking for libgtksourceview...")
-@static if is_linux()
-    try
-        strip(readall(pipeline(`ldconfig -p`, `grep libgtksourceview-3`, `cut -d'>' -f2`)))
-    catch
-        run(`sudo apt-get install libgtksourceview-3.0-1`)
-    end
-end
-@static if is_apple()
-    using Homebrew
-    if !Homebrew.installed("gtksourceview3")
-        Homebrew.add("gtksourceview3")
-    end
-    if !Homebrew.linked("gtksourceview3")
-        Homebrew.link("gtksourceview3")
-    end
-end
-
-
 using BinDeps, Compat
 
 @BinDeps.setup
